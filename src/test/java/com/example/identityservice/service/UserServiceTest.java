4e07640f40943ce49a28fdf2dcb9c8b42e4012d2
tthanh183 <trantanthanh29@gmail.com>
2025-02-10 14:52:46 +0700

    feat: userservice unit test

---
+++ src/test/java/com/example/identityservice/service/UserServiceTest.java
@@ -0,0 +1,90 @@
+package com.example.identityservice.service;
+
+import com.example.identityservice.dto.request.UserCreationRequest;
+import com.example.identityservice.dto.response.UserResponse;
+import com.example.identityservice.entity.User;
+import com.example.identityservice.exception.AppException;
+import com.example.identityservice.exception.ErrorCode;
+import com.example.identityservice.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    private UserCreationRequest request;
+    private UserResponse response;
+    private User user;
+    private LocalDate dob;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(2005, 2, 1);
+        request = UserCreationRequest.builder()
+                .username("tomdoe")
+                .password("12345678")
+                .firstName("Tom")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+
+        response = UserResponse.builder()
+                .id("1")
+                .username("tomdoe")
+                .firstName("Tom")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+
+        user = User.builder()
+                .id("1")
+                .username("tomdoe")
+                .firstName("Tom")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() {
+        // Given
+        Mockito.when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        // When
+        UserResponse actual = userService.createUser(request);
+
+        // Then
+        assertThat(actual.getId()).isEqualTo(user.getId());
+        assertThat(actual.getUsername()).isEqualTo(user.getUsername());
+        assertThat(actual.getFirstName()).isEqualTo(user.getFirstName());
+        assertThat(actual.getLastName()).isEqualTo(user.getLastName());
+    }
+
+    @Test
+    void createUser_userExisted_fail() {
+        // Given
+        Mockito.when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        // When, Then
+        var exception = assertThrows(AppException.class, () -> userService.createUser(request));
+        assertThat(exception.getErrorCode().getCode()).isEqualTo(ErrorCode.USER_EXISTED.getCode());
+    }
+
+}
